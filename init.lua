ab6dbe59f20dd394d8a58700baf8624f4c5adf9d
zjs <tyoukajyu@gmail.com>
2022-09-18 21:23:24 +0900

    nvim-autopairs

--- init.lua
+++ init.lua
@@ -1,25 +1,24 @@
 -- 基础配置
-require('basic')
+require("basic")
 -- Packer 插件管理
-require('plugins')
+require("plugins")
 -- 键位绑定
-require('keybindings')
+require("keybindings")
 -- 主题设置
-require('colorscheme')
+require("colorscheme")
 -- 插件配置
-require('plugin-config.nvim-tree')
-require('plugin-config.bufferline')
-require('plugin-config.lualine')
-require('plugin-config.telescope')
-require('plugin-config.dashboard')
-require('plugin-config.project')
-require('plugin-config.nvim-treesitter')
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
 require("plugin-config.indent-blankline")
+require("plugin-config.nvim-autopairs")
 -- 内置LSP
-require('lsp.setup')
-require('lsp.cmp')
-require('lsp.ui')
+require("lsp.setup")
+require("lsp.cmp")
+require("lsp.ui")
 -- require('lsp.formatter')
 require("lsp.null-ls")
-
-
