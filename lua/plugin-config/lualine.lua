725e110f31bde854498d823628afc0c727811fc3
TyouKajyu <tyoukajyu@gmail.com>
2022-11-24 01:43:13 +0900

    change colorscheme from tokyonight to onedark

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -7,7 +7,8 @@ end
 
 lualine.setup({
     options = {
-        theme = "tokyonight",
+        -- theme = "tokyonight",
+        theme = "onedark",
         component_separators = { left = "|", right = "|" },
         -- https://github.com/ryanoasis/powerline-extra-symbols
         section_separators = { left = " ", right = "" },
